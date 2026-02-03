572641262fd4e51dc114e277377ef4347d9cbae5
ksanabriad <ksanabriad@gmail.com>
2026-02-03 16:43:26 +0100

    gestion finanzas dashboard

--- src/app/services/finanzas.service.ts
+++ src/app/services/finanzas.service.ts
@@ -82,4 +82,35 @@ export class FinanzasService {
     public getMedios(): Observable<Array<any>> {
         return this.http.get<Array<any>>(`${environment.backendHost}/api/finanzas/medios`);
     }
+    public eliminarUsuario(username: string): Observable<void> {
+        return this.http.delete<void>(`${environment.backendHost}/api/usuarios/${username}`);
+    }
+
+    public getBalance(username: string): Observable<number> {
+        return this.http.get<number>(`${environment.backendHost}/api/finanzas/${username}/balance`);
+    }
+
+    public getLast3MonthsExpenses(username: string): Observable<Array<any>> {
+        return this.http.get<Array<any>>(`${environment.backendHost}/api/finanzas/${username}/expenses-last-3-months`);
+    }
+
+    public getLast3MonthsIncome(username: string): Observable<Array<any>> {
+        return this.http.get<Array<any>>(`${environment.backendHost}/api/finanzas/${username}/income-last-3-months`);
+    }
+
+    public getCurrentMonthIncome(username: string): Observable<number> {
+        return this.http.get<number>(`${environment.backendHost}/api/finanzas/${username}/current-month-income`);
+    }
+
+    public getCurrentMonthExpenses(username: string): Observable<number> {
+        return this.http.get<number>(`${environment.backendHost}/api/finanzas/${username}/current-month-expenses`);
+    }
+
+    public getCategoryDistribution(username: string): Observable<Array<any>> {
+        return this.http.get<Array<any>>(`${environment.backendHost}/api/finanzas/${username}/category-distribution`);
+    }
+
+    public getRecentTransactions(username: string): Observable<Array<any>> {
+        return this.http.get<Array<any>>(`${environment.backendHost}/api/finanzas/${username}/recent`);
+    }
 }
