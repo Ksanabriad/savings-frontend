87f872309249056b08855eea8cafe4530f97f936
ksanabriad <ksanabriad@gmail.com>
2026-02-05 18:18:35 +0100

    deleting file

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css',
+  templateUrl: './app.html',
 })
+
 export class App {
   protected readonly title = signal('savings-frontend');
 }
